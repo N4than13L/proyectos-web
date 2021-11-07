36d713cec232c11b990ec3ed9a34c10bc1dc10bf
José Nathaniel Bonilla <75401135+N4than13L@users.noreply.github.com>
2021-11-07 19:05:30 -0400

    cambiando nombre del proyecto y rejustando el index

---
+++ dentistas/js/index.js
@@ -0,0 +1,8 @@
+// mobile menu
+
+const bugerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+bugerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active');
+});
