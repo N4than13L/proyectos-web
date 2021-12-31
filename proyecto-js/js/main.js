487bcbad94df8058673ad247693caa41989c4b04
José Nathaniel Bonilla <75401135+N4than13L@users.noreply.github.com>
2021-12-31 15:28:40 -0400

    mejorando estilos y agregando sliders y formulario

---
+++ proyecto-js/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+     $('.galeria').bxSlider({
+          mode: 'fade',
+          captions: true,
+          slideWidth: 1200,
+          responsive: true,
+          pager: false
+     });
+       
+})
